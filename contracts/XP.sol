085b4d33034b0194d9f62ab46e4b98ee276e46fa
egeaybars123 <egeaybars1102@gmail.com>
2024-03-08 19:11:43 +0300

    only xp contract

--- contracts/XP.sol
+++ contracts/XP.sol
@@ -4,22 +4,12 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//0x8b9B061e386D460683f0CC137FFD03f28229365E
+//0x8C9825d7fA0811b60D0Ca244B2AEad630846EF05
 contract LoCXP is ERC20, Ownable {
-    address public CHEST;
 
     constructor() ERC20("Land of Chasers XP", "LoC-XP") Ownable(msg.sender) {}
 
-    function mintXPInventory(address _to, uint256 _amount) external {
-        require(msg.sender == CHEST, "Not authorized.");
+    function mintXP(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
-    }
-
-    function mintXPKill(address _to, uint256 _amount) external onlyOwner {
-        _mint(_to, _amount);
-    }
-
-    function updateChestAddress(address chestAddr) external onlyOwner {
-        CHEST = chestAddr;
     }
 }
